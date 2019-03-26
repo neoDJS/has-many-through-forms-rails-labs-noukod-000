0be5fadd5ce106ee7f51eeae2f91043a72451462
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-26 15:34:30 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 
+  def user_name
+    self.user.name
+  end
 end
